a60c0df10a8d3319fc26f0495173fd59f2273ece
vincentdu101 <vincentdu101@gmail.com>
2013-12-23 19:07:19 -0800

    Initial testing

---
+++ user-files/simulations/basic/BasicExampleSimulation.scala
@@ -0,0 +1,56 @@
+package basic
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import io.gatling.http.Headers.Names._
+import scala.concurrent.duration._
+import bootstrap._
+import assertions._
+import com.ning.http.client._
+import java.util.concurrent.Future
+
+class BasicExampleSimulation extends Simulation {
+
+	val client = new AsyncHttpClient()
+  val response = client.prepareGet("test").execute().get()
+  println(response.getResponseBody())
+
+
+	val httpProtocol = http
+		.baseURL("test")
+		.acceptCharsetHeader("ISO-8859-1,utf-8;q=0.7,*;q=0.7")
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+		.disableFollowRedirect
+
+
+	val headers_1 = Map(
+		"Keep-Alive" -> "115")
+
+	val headers_3 = Map(
+		"Keep-Alive" -> "115",
+		"Content-Type" -> "application/x-www-form-urlencoded")
+
+	val headers_6 = Map(
+		"Accept" -> "application/json, text/javascript, */*; q=0.01",
+		"Keep-Alive" -> "115",
+		"X-Requested-With" -> "XMLHttpRequest")
+
+	val scn = scenario("Scenario name")
+		.group("Ads") {
+			exec(
+				http("get ad unit")
+					.get("/db98791e231ad2c925fdbb39")
+					.headers(headers_1)
+					.check(status.is(200)))
+				.pause(0 milliseconds, 100 milliseconds)
+		}
+
+	setUp(scn.inject(ramp(100 users) over (5 seconds)))
+		.protocols(httpProtocol)
+		.assertions(
+			global.successfulRequests.percent.is(100), details("Login" / "request_2").responseTime.max.lessThan(2000),
+			details("request_9").requestsPerSec.greaterThan(10))
+}
