770d82ccd0104073d202999cb848e0d1bb01af4f
mohammad <mqaffaf@onecreen.com>
2013-12-27 19:14:31 -0800

    Initial progress

--- user-files/simulations/basic/ParseXMLRegex.scala
+++ user-files/simulations/basic/ParseXMLRegex.scala
@@ -31,3 +31,31 @@ class ParseXMLRegex {
   }
 
 }
+try {
+  File file = new File("http://a.goember.com/vast/db98791e231ad2c925fdbb39");
+       BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+       System.out.print("Enter XML File name: ");
+       String xmlFile = bf.readLine();
+       File file = new File(xmlFile);
+       if(file.exists()){
+         // Create a factory
+         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+         // Use the factory to create a builder
+         DocumentBuilder builder = factory.newDocumentBuilder();
+         Document doc = builder.parse(xmlFile);
+         // Get a list of all elements in the document
+         NodeList list = doc.getElementsByTagName("*");
+         System.out.println("XML Elements: ");
+         for (int i=0; i<list.getLength(); i++) {
+           // Get element
+           Element element = (Element)list.item(i);
+           System.out.println(element.getNodeName());
+         }
+       }
+       else{
+         System.out.print("File not found!");
+       }
+     }
+     catch (Exception e) {
+       System.exit(1);
+     }
